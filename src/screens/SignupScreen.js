5776415e7eec1a2550287b7fa78732294a963f62
Marek Kaszycki <marek.kaszycki@northmill.se>
2022-01-28 07:59:27 +0100

    Removed header, styled Signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,19 +5,31 @@ import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Wellcome! Please sign up</Text>
       </Spacer>
-      <Input label="Email"/>
-      <Input label="Password"/>
+      <Input label="Email" />
+      <Input label="Password" />
       <Spacer>
-        <Button title="Sign Up"/>
+        <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  }
+});
 
 export default SignupScreen;
